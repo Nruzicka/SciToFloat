5c6a196b080109ed82d89090c9081f8257efe825
Nruzicka <nruzicka.nr@gmail.com>
2023-11-09 11:18:34 +0800

    First commit.

---
+++ SciToExpNo.py
@@ -0,0 +1,21 @@
+import re
+
+#Returns the amount of decimals the number should contain. 
+def get_precision(sci_num: str) -> int:
+    base, exp = sci_num.split("e")
+    if int(exp) < 0:
+        if "." in base:
+            base_dec = len(base.split(".")[1])
+        else:
+            base_dec = 0
+        return int(abs(int(exp)) + base_dec)
+
+#Takes a number (in string value) in scientific notation and changes it to a float. 
+def exp_to_float(val: str) -> str:
+    if re.match(r'[-+?[0-9]*\.?[0-9]+[Ee][-+]?[0-9]+', val):
+        try:
+            precision = get_precision(val)
+            return f"{float(val):.{precision}f}"
+        except ValueError:
+            pass
+    return val
